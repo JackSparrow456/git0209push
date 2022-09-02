ee28f9c30d4b41106c283c1296f51ed49ffcb144
JackSparrow <sherlockholmes001.in@gmail.com>
2022-09-02 19:27:46 +0530

    task

---
+++ Class.java
@@ -0,0 +1,20 @@
+package org.cts;
+
+public class Class {
+	
+	public static int age;
+public static void id() {
+	int a= 10;
+	System.out.println(a);
+}
+	
+public static void main(String[] args) {
+id();
+System.out.println(age);
+
+	
+	
+}
+
+
+}
---
+++ Student.java
@@ -0,0 +1,18 @@
+package org.cts;
+
+public class Student extends Class {
+
+	
+	public static void main(String[] args) {
+		
+		//with extends
+		id();
+		System.out.println(age);
+		
+		//withoutextends
+		
+		Class.id();
+		System.out.println(Class.age);
+	}
+	
+}
